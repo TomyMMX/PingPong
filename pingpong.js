386053cbdb46c85f7c171211688445efc9c5e83c
Tomaz Toplak <tomaz.toplak@gmail.com>
2014-03-25 04:03:41 +0100

    Ping Pong

---
+++ pingpong.js
@@ -0,0 +1,21 @@
+function getPingPongString(){
+  var outString = ''
+  for(var i=1;i<=100;i++){
+    var iStr=i+'';	
+    if(i%35===0||(iStr.indexOf('7')!=-1&&iStr.indexOf('5')!=-1)){
+    	outString+='PINGPONG'      
+    }
+    else if(i%7===0||iStr.indexOf('7')!=-1){
+      outString+='PONG'
+    }
+    else if(i%5===0||iStr.indexOf('5')!=-1){
+      outString+='PING'
+    }	
+    else{
+      outString+= i
+    }
+    
+    outString+='<br />'
+  }
+  document.write(outString);
+}
